e22cd7e8b706ce5f46696bf2ac784df765a7b079
Elizabeth <ekcarney@smith.edu>
2020-07-30 16:53:23 -0500

    working with one-dimensional data and padding system

--- source/web/demo.cc
+++ source/web/demo.cc
@@ -4,27 +4,60 @@
 #include "../../third-party/Empirical/source/web/d3/scales.h"
 #include "../../third-party/Empirical/source/web/d3/axis.h"
 
+/* struct ExampleJSONData { EMP_BUILD_INTROSPECTIVE_TUPLE( int, x, int, y, std::string, name ) };
+int get_x(ExampleJSONData d, int i, int j) { return d.x(); };
+int get_y(ExampleJSONData d, int i, int j) { return d.y(); };
+std::string get_name(ExampleJSONData d, int i, int j) { return d.name(); }; */
+
+std::function<int(int, int, int)> return_data = [](int d, int i, int j){ return d; };
+
 void MakeScatterplot() {
 
-  std::string x_label = "X Axis";
-  std::string y_label = "Y Axis";
+  //emp::vector<emp::vector<int>> example_data = {{10,100}, {15,150}, {20,250}, {40,275}, {50,280}, {50,300}, {90,400}};
+  emp::vector<int> example_data = {10, 15, 20, 40, 50, 60, 90};
 
-  std::string margin = "70";
-  double svg_width = 500;
+  //D3::Dataset dataset;
+  //dataset.LoadDataFromFile(example_data.json);
+
+  std::string x_label = "Age";
+  std::string y_label = "Total Ice Cream Cones Eaten";
+
+  double svg_width = 700;
   double svg_height = 500;
+  double graph_width = 600;
+  double graph_height = 400;
 
+  double padding_bottom = svg_height - graph_height;
+  double padding_left = 60;
+
+  double data_point_radius = 5;
+
+  // set up div and svg
   D3::Selection viz_div = D3::Select("body").Append("div").SetAttr("id", "scatterplot_div");
   D3::Selection viz_svg = D3::Select("#scatterplot_div").Append("svg").SetAttr("id", "scatterplot_svg")
                           .SetAttr("width", svg_width).SetAttr("height", svg_height);
 
+  // set up scales
   D3::LinearScale x_scale = D3::LinearScale();
-  x_scale.SetDomain(0, 100).SetRange(0, 600);
+  x_scale.SetDomain(0, 100).SetRange(0, graph_width);
   D3::LinearScale y_scale = D3::LinearScale();
-  y_scale.SetDomain(0, 100).SetRange(0, 400);
+  y_scale.SetDomain(0, 100).SetRange(graph_height, 0);
 
-  D3::Axis<D3::LinearScale> bottom_axis = D3::Axis<D3::LinearScale>("bottom", x_label, 60).SetScale(x_scale);
-  D3::Axis<D3::LinearScale> left_axis = D3::Axis<D3::LinearScale>("left", y_label, 80).SetScale(y_scale);
+  // set up axes with padding
+  D3::Axis<D3::LinearScale> bottom_axis = D3::Axis<D3::LinearScale>("bottom", x_label, padding_bottom).SetScale(x_scale);
+  D3::Axis<D3::LinearScale> left_axis = D3::Axis<D3::LinearScale>("left", y_label, padding_left).SetScale(y_scale);
   D3::DrawAxes(bottom_axis, left_axis, viz_svg);
+
+  // set up circle data points
+  D3::Selection data_points = viz_svg.SelectAll("circle");
+  // bind data and assign a circle to each data point without a DOM element
+  // (use lambdas for ApplyScale so that it's in scope)
+  D3::Selection circles = viz_svg.SelectAll("circle")
+          .Data(example_data, return_data)
+          .EnterAppend("circle")
+          .SetAttr("cx", [&padding_left, &x_scale](int d, int i, int j) { return padding_left + x_scale.ApplyScale<int, int>(d); })
+          .SetAttr("cy", [&y_scale](int d, int i, int j) { return y_scale.ApplyScale<int, int>(d); })
+          .SetAttr("r", data_point_radius);
 
 }
 
